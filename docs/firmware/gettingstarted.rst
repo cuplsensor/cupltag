44207ed84d9961a32349ef028de4e3c4a0415aa3
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-05-26 11:58:15 +0100

    Docs

--- docs/firmware/gettingstarted.rst
+++ docs/firmware/gettingstarted.rst
@@ -6,8 +6,8 @@ Prerequisites
 * MSP-FET debugger (`TI <https://www.ti.com/tool/MSP-FET>`_).
 * GitHub Desktop (`download <https://desktop.github.com/>`_) or your choice of Git software.
 
-Fork and Clone the cupl Tag repository
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Fork and Clone cupl Tag
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .. image:: fork-and-clone.png
    :width: 400
@@ -21,23 +21,25 @@ Clone the forked repository to your computer by clicking the green Clone or Down
 Open the Code Composer Project
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#. (`Download <https://software-dl.ti.com/ccs/esd/documents/ccs_downloads.html>`_) Code Composer Studio 10 (CCS).
+#. `Download <https://software-dl.ti.com/ccs/esd/documents/ccs_downloads.html>`_ and install Code Composer Studio 10 (CCS).
 #. Open CCS. Launch the workspace of your choice.
 #. Click File -> Open Projects from File System...
-#. In Import Source, select the local directory you cloned cupl Tag into earlier.
+#. In Import Source, select the folder containing the clone of cupl Tag.
 
-.. image:: ccsimport.png
-   :width: 400
+    .. image:: ccsimport.png
+       :width: 400
 
 #. Ensure that cupltag\\firmware is **checked**. Every other folder should be unchecked.
 #. Click Finish.
 #. The project is now open.
 
-.. image:: ccsopen.png
-   :width: 400
+    .. image:: ccsopen.png
+       :width: 400
 
-Add the Reference to cupl Codec
+Add Reference to cupl Codec
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The cupl Tag firmware depends on C files from cupl Codec.
 
 #. Fork the cupl Codec repository on (`GitHub <https://github.com/cuplsensor/cuplcodec>`_).
 #. Clone this into a folder on your computer.
@@ -45,17 +47,17 @@ Add the Reference to cupl Codec
 #. Select Properties from the context menu.
 #. In the Properties window, expand Resource on the left hand panel and select Linked Resources.
 
-.. image:: ccslinkedresources.png
-   :width: 400
+    .. image:: ccslinkedresources.png
+       :width: 400
 
 #. Double click the CUPLCODEC path variable. The Edit Variable window will appear.
 
-.. image:: ccseditpathvar.png
-   :width: 400
+    .. image:: ccseditpathvar.png
+       :width: 400
 
-#. Click the Folder... button. Select the Codec folder from step 2.
+#. Click the Folder... button. Select the Codec clone folder from step 2.
 #. Click Apply and Close.
 #. The cuplcodec_encoder project folder will now contain references to files inside cupl Codec.
 
-.. image:: ccsreferenceadded.png
-   :width: 400
+    .. image:: ccsreferenceadded.png
+       :width: 400
--- firmware/main.c
+++ firmware/main.c
@@ -1,34 +1,3 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2016, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
 #include "driverlib.h"
 #include "hdc2010.h"
 #include "sample.h"
@@ -39,54 +8,6 @@
 #include "defs.h"
 #include <stdlib.h>
 
-//*****************************************************************************
-//! This example shows how to configure the I2C module as a master for
-//! single byte transmission in interrupt mode. The address of the slave
-//! module that the master is communicating with also set in this example.
-//!
-//! Demo - EUSCI_B0 I2C Master TX single bytes to MSP430 Slave
-//!
-//!  Description: This demo connects two MSP430's via the I2C bus. The master
-//!  transmits to the slave. This is the master code. It continuously
-//!  transmits 00h, 01h, ..., 0ffh and demonstrates how to implement an I2C
-//!  master transmitter sending a single byte using the USCI_B0 TX interrupt.
-//!  ACLK = n/a, MCLK = SMCLK = BRCLK = default DCO = ~1.045MHz
-//!
-//!                                /|\  /|\
-// //!                MSP430FR5969    10k  10k     MSP430FR5969
-//!                   slave         |    |         master
-//!             -----------------   |    |   -----------------
-//!           -|XIN  P1.6/UCB0SDA|<-|----+->|P1.6/UCB0SDA  XIN|-
-//!            |                 |  |       |                 |
-//!           -|XOUT             |  |       |             XOUT|-
-//!            |     P1.7/UCB0SCL|<-+------>|P1.7/UCB0SCL     |
-//!            |                 |          |                 |
-//!
-//! This example uses the following peripherals and I/O signals.  You must
-//! review these and change as needed for your own board:
-//! - I2C peripheral
-//! - GPIO Port peripheral (for I2C pins)
-//! - SCL2
-//! - SDA
-//!
-//! This example uses the following interrupt handlers.  To use this example
-//! in your own application you must add these interrupt handlers to your
-//! vector table.
-//! - USCI_B0_VECTOR.
-//!
-//
-//*****************************************************************************
-//*****************************************************************************
-//
-//Set the address for slave module. This is a 7-bit address sent in the
-//following format:
-//[A6:A5:A4:A3:A2:A1:A0:RS]
-//
-//A zero in the "RS" position of the first byte means that the master
-//transmits (sends) data to the selected slave, and a one in this position
-//means that the master receives data from the slave.
-//
-//*****************************************************************************
 #define SLAVE_ADDRESS 0x5F
 
 volatile uint8_t receiveData;
